e44dc255c35593e150071aef7891fc89ede846b4
GameP-lu <69840113+GameP-lu@users.noreply.github.com>
2021-01-17 00:24:41 +0900

    Add files via upload

---
+++ src/js/data/2019-11-26.js
@@ -0,0 +1,39 @@
+dataSetVersion = "2019-11-26"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "리들 로즈하트",
+    img: "D2HAHqt.png",
+    opts: {
+    }
+  },
+  {
+    name: "트레이 클로버",
+    img: "2E0H7Rf.png",
+    opts: {
+    }
+  },
+  {
+    name: "케이터 다이아몬드",
+    img: "U4Tj2We.png",
+    opts: {
+    }
+  },
+  {
+    name: "듀스 스페이드",
+    img: "OcvePoO.png",
+    opts: {
+      nameless: true
+    },
+  },
+  {
+    name: "에이스 트라폴라",
+    img: "zoUdqev.png",
+    opts: {
+    }
+  },
+];

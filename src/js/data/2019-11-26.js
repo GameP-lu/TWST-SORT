5cf6296ae730f0396549b426299e92af5e50ed27
GameP-lu <69840113+GameP-lu@users.noreply.github.com>
2021-01-17 01:11:18 +0900

    Update 2019-11-26.js

--- src/js/data/2019-11-26.js
+++ src/js/data/2019-11-26.js
@@ -27,12 +27,113 @@ dataSet[dataSetVersion].characterData = [
     name: "듀스 스페이드",
     img: "OcvePoO.png",
     opts: {
-      nameless: true
     },
   },
   {
     name: "에이스 트라폴라",
     img: "zoUdqev.png",
+    opts: {
+    }
+  },
+  {
+    name: "레오나 킹스칼라",
+    img: "NoaH9BF.png",
+    opts: {
+    }
+  },
+  {
+    name: "라기 부치",
+    img: "p423JHr.png",
+    opts: {
+    }
+  },
+  {
+    name: "잭 하울",
+    img: "tQwvBDd.png",
+    opts: {
+    }
+  },
+  {
+    name: "아즐 아셴그로토",
+    img: "OkQsurU.png",
+    opts: {
+    }
+  },
+  {
+    name: "제이드 리치",
+    img: "HALQzZW.png",
+    opts: {
+    }
+  },
+  {
+    name: "플로이드 리치",
+    img: "QlGMk1T.png",
+    opts: {
+    }
+  },
+  {
+    name: "카림 알 아짐",
+    img: ".png",
+    opts: {
+    }
+  },
+  {
+    name: "쟈밀 바이퍼",
+    img: ".png",
+    opts: {
+    }
+  },
+  {
+    name: "빌 셴하이트",
+    img: ".png",
+    opts: {
+    }
+  },
+  {
+    name: "루크 헌트",
+    img: ".png",
+    opts: {
+    }
+  },
+  {
+    name: "에펠 펠미에",
+    img: ".png",
+    opts: {
+    }
+  },
+  {
+    name: "이데아 슈라우드",
+    img: ".png",
+    opts: {
+    }
+  },
+  {
+    name: "오르토 슈라우드",
+    img: ".png",
+    opts: {
+    }
+  },
+  {
+    name: "말레우스 드라코니아",
+    img: ".png",
+    opts: {
+    }
+  },
+  {
+    name: "릴리아 반루즈",
+    img: ".png",
+    opts: {
+    }
+  },
+  {
+    name: "세벡 지그볼트",
+    img: ".png",
+    opts: {
+    }
+  },
+  {
+    name: "실버",
+    img: ".png",
     opts: {
     }
   },
